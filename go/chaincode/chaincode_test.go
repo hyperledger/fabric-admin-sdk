0d1d23d9c0168ff6789b150f58e94945f422a0f5
Sam Yuan <yy19902439@126.com>
2022-06-20 21:10:28 +0800

    add e2e case and unit test targets
    Signed-off-by: Sam Yuan <yy19902439@126.com>

---
+++ go/chaincode/chaincode_test.go
@@ -0,0 +1,19 @@
+package chaincode_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("Chaincode", func() {
+
+	PIt("Chaincode package", func() {})
+	PIt("Chaincode install", func() {})
+	PIt("Chaincode queryinstalled", func() {})
+	PIt("Chaincode getinstalledpackage", func() {})
+	PIt("Chaincode calculatepackageid", func() {})
+	PIt("Chaincode approveformyorg", func() {})
+	PIt("Chaincode queryapproved", func() {})
+	PIt("Chaincode checkcommitreadiness", func() {})
+	PIt("Chaincode commit", func() {})
+	PIt("Chaincode querycommitted", func() {})
+})
---
+++ go/channel/channel_test.go
@@ -0,0 +1,14 @@
+package channel_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("Channel", func() {
+	PIt("Create new channel", func() {})
+	PIt("Update channel", func() {})
+	PIt("Peer join channel", func() {})
+	PIt("Peer exit channel", func() {})
+	PIt("List channel", func() {})
+	PIt("Inspect channel", func() {})
+})
