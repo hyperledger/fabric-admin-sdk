49787bd2c71486d8134cca8461d8fde8a1069dac
Sam Yuan <yy19902439@126.com>
2022-06-21 23:21:32 +0800

    add e2e case and unit test targets (#13)
    Signed-off-by: Sam Yuan <yy19902439@126.com>

---
+++ go/tools/tools_test.go
@@ -0,0 +1,11 @@
+package tools_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+)
+
+var _ = Describe("Tools", func() {
+	PIt("gate policy", func() {})
+	PIt("peer discovery", func() {})
+	PIt("generate connection profile for sdk", func() {})
+})
