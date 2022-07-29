ffca4f0ffb19595be4974ae5b23fdcf36840caf0
Sam Yuan <yy19902439@126.com>
2022-07-29 23:01:14 +0800

    impl for ccaas chaincode package resolve #23
    Signed-off-by: Sam Yuan <yy19902439@126.com>

--- go/chaincode/chaincode.go
+++ go/chaincode/chaincode.go
@@ -1,1 +1,84 @@
 package chaincode
+
+import (
+	"archive/tar"
+	"bytes"
+	"encoding/json"
+)
+
+type Connection struct {
+	Address      string `json:"address"`
+	Dial_timeout string `json:"dial_timeout"`
+	Tls_required bool   `json:"tls_required"`
+}
+
+type Metadata struct {
+	Type  string `json:"type"`
+	Label string `json:"label"`
+}
+
+type TarFile struct {
+	Name string
+	Body string
+}
+
+/*
+To use PackageCCAAS you need to start container by yourself. as sample as
+${CONTAINER_CLI} run --rm -d --name peer0org1_${CC_NAME}_ccaas  \
+--network fabric_test \
+-e CHAINCODE_SERVER_ADDRESS=0.0.0.0:${CCAAS_SERVER_PORT} \
+-e CHAINCODE_ID=$PACKAGE_ID -e CORE_CHAINCODE_ID_NAME=$PACKAGE_ID \
+${CC_NAME}_ccaas_image:latest
+*/
+func PackageCCAAS(connection Connection, metadata Metadata) (string, error) {
+	connjsonStr, err := json.Marshal(connection)
+	if err != nil {
+		return "", err
+	}
+	connectionTarFile := TarFile{
+		Name: "connection.json",
+		Body: string(connjsonStr),
+	}
+	codeTarString, err := createTar([]TarFile{connectionTarFile})
+	if err != nil {
+		return "", err
+	}
+	metajsonStr, err := json.Marshal(metadata)
+	if err != nil {
+		return "", err
+	}
+	metaTarFile := TarFile{
+		Name: "metadata.json",
+		Body: string(metajsonStr),
+	}
+	codeTarFile := TarFile{
+		Name: "code.tar.gz",
+		Body: codeTarString,
+	}
+	//tar -C "$tempdir/pkg" -czf "$CC_NAME.tar.gz" metadata.json code.tar.gz
+	finalString, err := createTar([]TarFile{codeTarFile, metaTarFile})
+	if err != nil {
+		return "", err
+	}
+	return finalString, nil
+}
+
+func createTar(files []TarFile) (string, error) {
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+
+	for _, file := range files {
+		hdr := &tar.Header{
+			Name: file.Name,
+			Mode: 0600,
+			Size: int64(len(file.Body)),
+		}
+		if err := tw.WriteHeader(hdr); err != nil {
+			return "", err
+		}
+		if _, err := tw.Write([]byte(file.Body)); err != nil {
+			return "", err
+		}
+	}
+	return buf.String(), nil
+}
--- go/chaincode/chaincode_test.go
+++ go/chaincode/chaincode_test.go
@@ -1,12 +1,63 @@
 package chaincode_test
 
 import (
+	"archive/tar"
+	"fabric-admin-sdk/chaincode"
+	"io"
+	"log"
+	"os"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Chaincode", func() {
 
-	PIt("Chaincode package", func() {})
+	It("Chaincode package ccaas", func() {
+		dummyConnection := chaincode.Connection{
+			Address:      "127.0.0.1:8080",
+			Dial_timeout: "10s",
+			Tls_required: false,
+		}
+		dummyMeta := chaincode.Metadata{
+			Type:  "ccaas",
+			Label: "basic-asset",
+		}
+		str, err := chaincode.PackageCCAAS(dummyConnection, dummyMeta)
+		Expect(err).NotTo(HaveOccurred())
+		// so far no plan to verify the file
+		file, err := os.OpenFile(tmpDir+"/chaincode.tar.gz", os.O_CREATE|os.O_WRONLY, os.ModePerm)
+		Expect(err).NotTo(HaveOccurred())
+		_, err = file.WriteString(str)
+		Expect(err).NotTo(HaveOccurred())
+		file.Close()
+
+		file, err = os.Open(tmpDir + "/chaincode.tar.gz")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer file.Close()
+		tr := tar.NewReader(file)
+		i := 0
+		for {
+			hdr, err := tr.Next()
+			if err == io.EOF {
+				break
+			}
+			Expect(err).NotTo(HaveOccurred())
+			if i == 0 {
+				Expect(hdr.Name).To(Equal("code.tar.gz"))
+			}
+			if i == 1 {
+				Expect(hdr.Name).To(Equal("metadata.json"))
+			}
+			i++
+		}
+	})
+
+	PIt("Chaincode package java", func() {})
+	PIt("Chaincode package golang", func() {})
+	PIt("Chaincode package nodejs", func() {})
 	PIt("Chaincode install", func() {})
 	PIt("Chaincode queryinstalled", func() {})
 	PIt("Chaincode getinstalledpackage", func() {})
