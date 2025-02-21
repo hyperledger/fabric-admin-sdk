c1a00123fe0711fd4e2af6a7b859c4c2e08efcf4
Ricardo Santos <180027263@aluno.unb.br>
2025-02-21 14:21:58 -0300

    Rename Client struct to Peer, to better relfect the component the client is connecting to
    Properly rename New constructor to NewPeer to better relfect the component it is creating
    
    Rename Client methods by removing the Snapshots subtr to make it clearer
    
    Fix QueryPending snapshots method to return QueryPendingSnapshotsResponse
    
    Signed-off-by: Ricardo Santos <180027263@aluno.unb.br>

--- pkg/snapshot/snapshot.go
+++ pkg/snapshot/snapshot.go
@@ -12,64 +12,65 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-// Client is a wrapper around snapshot client
-type Client struct {
+// Peer is a wrapper around snapshot client
+type Peer struct {
 	snapshot peer.SnapshotClient
 	id       identity.SigningIdentity
 }
 
-func New(conn grpc.ClientConnInterface, id identity.SigningIdentity) *Client {
-	return &Client{
+func NewPeer(conn grpc.ClientConnInterface, id identity.SigningIdentity) *Peer {
+	return &Peer{
 		snapshot: peer.NewSnapshotClient(conn),
 		id:       id,
 	}
 }
 
-// SubmitSnapshotRequest from a specific peer
-func (s *Client) SubmitSnapshotRequest(ctx context.Context, channelID string, blockNum uint64) error {
-	signedRequest, err := s.newSignedSnapshotRequest(channelID, blockNum)
+// SubmitRequest snapshot from a specific peer
+func (p *Peer) SubmitRequest(ctx context.Context, channelID string, blockNum uint64) error {
+	signedRequest, err := p.newSignedSnapshotRequest(channelID, blockNum)
 	if err != nil {
 		return fmt.Errorf("failed to create signed snapshot request: %w", err)
 	}
 
-	if _, err := s.snapshot.Generate(ctx, signedRequest); err != nil {
+	if _, err := p.snapshot.Generate(ctx, signedRequest); err != nil {
 		return fmt.Errorf("failed to submit generate snapshot request: %w", err)
 	}
 
 	return nil
 }
 
-// CancelSnapshotRequest from a specific peer
-func (s *Client) CancelSnapshotRequest(ctx context.Context, channelID string) error {
-	signedRequest, err := s.newSignedSnapshotRequest(channelID, 0)
+// CancelRequest snapshot from a specific peer
+func (p *Peer) CancelRequest(ctx context.Context, channelID string) error {
+	signedRequest, err := p.newSignedSnapshotRequest(channelID, 0)
 	if err != nil {
 		return fmt.Errorf("failed to create signed snapshot request: %w", err)
 	}
 
-	if _, err := s.snapshot.Cancel(ctx, signedRequest); err != nil {
+	if _, err := p.snapshot.Cancel(ctx, signedRequest); err != nil {
 		return fmt.Errorf("failed to cancel snapshot request: %w", err)
 	}
 
 	return nil
 }
 
-// ListPendingSnapshots from a specific peer
-func (s *Client) ListPendingSnapshots(ctx context.Context, channelID string) error {
-	signedRequest, err := s.newSignedSnapshotRequest(channelID, 0)
+// QueryPending snapshots from a specific peer
+func (p *Peer) QueryPending(ctx context.Context, channelID string) (*peer.QueryPendingSnapshotsResponse, error) {
+	signedRequest, err := p.newSignedSnapshotRequest(channelID, 0)
 	if err != nil {
-		return fmt.Errorf("failed to create signed snapshot request: %w", err)
+		return nil, fmt.Errorf("failed to create signed snapshot request: %w", err)
 	}
 
-	if _, err := s.snapshot.QueryPendings(ctx, signedRequest); err != nil {
-		return fmt.Errorf("failed to list pending snapshots: %w", err)
+	res, err := p.snapshot.QueryPendings(ctx, signedRequest)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list pending snapshots: %w", err)
 	}
 
-	return nil
+	return res, nil
 }
 
 // newSignedSnapshotRequest returns a signed snapshot request for
 // given channel
-func (s *Client) newSignedSnapshotRequest(channelID string, blockNum uint64) (*peer.SignedSnapshotRequest, error) {
+func (p *Peer) newSignedSnapshotRequest(channelID string, blockNum uint64) (*peer.SignedSnapshotRequest, error) {
 	nonce, err := protoutil.CreateNonce()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create nonce: %w", err)
@@ -77,7 +78,7 @@ func (s *Client) newSignedSnapshotRequest(channelID string, blockNum uint64) (*p
 
 	request, err := proto.Marshal(&peer.SnapshotRequest{
 		SignatureHeader: &common.SignatureHeader{
-			Creator: s.id.Credentials(),
+			Creator: p.id.Credentials(),
 			Nonce:   nonce,
 		},
 		ChannelId:   channelID,
@@ -87,7 +88,7 @@ func (s *Client) newSignedSnapshotRequest(channelID string, blockNum uint64) (*p
 		return nil, fmt.Errorf("failed to marshal snapshot request: %w", err)
 	}
 
-	signature, err := s.id.Sign(request)
+	signature, err := p.id.Sign(request)
 	if err != nil {
 		return nil, fmt.Errorf("failed to sign snapshot request: %w", err)
 	}
